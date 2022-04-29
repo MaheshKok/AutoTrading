e097aeb431bd9d07df0f617c70f51434e9a6f901
MaheshKok <maheshkokare100@gmail.com>
2022-04-29 15:25:37 +0530

    forgot to add alice_blue.py changes

--- apis/broker/alice_blue.py
+++ apis/broker/alice_blue.py
@@ -15,6 +15,7 @@ log = logging.getLogger(__name__)
 
 def get_alice_blue_obj():
     from main import app
+
     with app.app_context():
         broker = Broker.query.filter_by(name="alice_blue").scalar()
         try:
@@ -25,7 +26,9 @@ def get_alice_blue_obj():
                 master_contracts_to_download=["NFO"],
             )
         except:
-            broker = Broker.query.filter_by(name="alice_blue").with_for_update().scalar()
+            broker = (
+                Broker.query.filter_by(name="alice_blue").with_for_update().scalar()
+            )
             access_token = AliceBlue.login_and_get_access_token(
                 username=broker.username,
                 password=broker.password,
@@ -47,7 +50,13 @@ def get_alice_blue_obj():
 
 
 def close_alice_blue_trades(
-    strike_quantity_dict, symbol, expiry: datetime.date, nfo_type
+    strike_quantity_dict,
+    symbol,
+    expiry: datetime.date,
+    nfo_type,
+    ongoing_trades,
+    data,
+    current_time,
 ):
     """
     assumptions
@@ -57,20 +66,22 @@ def close_alice_blue_trades(
       call type [ for ex: either CE, PE ]
       nfo type [ for ex: either future or option]
     """
+    from apis.utils import close_ongoing_trades
 
     alice = get_alice_blue_obj()
-    close_order_action_id_list = []
+    strike_option_type_close_order_id_dict = {}
 
     if isinstance(expiry, str):
         expiry = datetime.datetime.strptime(expiry, "%d %b %Y").date()
 
     for strike, quantity in strike_quantity_dict.items():
+        option_type = "ce" if quantity > 0 else "pe"
         instrument = alice.get_instrument_for_fno(
             symbol=symbol,
             expiry_date=expiry,
             is_fut=nfo_type != "option",
             strike=strike,
-            is_CE=quantity > 0,
+            is_CE=option_type == "ce",
         )
 
         place_order_response = alice.place_order(
@@ -86,20 +97,27 @@ def close_alice_blue_trades(
             trailing_sl=None,
             is_amo=False,
         )
-        close_order_action_id_list.append(place_order_response["data"]["oms_order_id"])
+        strike_option_type_close_order_id_dict[
+            f"{strike}_{option_type}"
+        ] = place_order_response["data"]["oms_order_id"]
 
+    strike_option_type_exit_price_dict = {}
     # check for this
-    # for order_id in close_order_action_id_list:
-    #     response = alice.get_order_history(order_id)
-    #     if response["status"] == "success":
-    #         continue
-    #     else:
-    #         print(response)
+    for strike_option_type, order_id in strike_option_type_close_order_id_dict.items():
+        order_history = alice.get_order_history(order_id)["data"][0]
+        if order_history["order_status"] == "complete":
+            strike_option_type_exit_price_dict[strike_option_type] = order_history[
+                "average_price"
+            ]
+        else:
+            print(order_history)
+
+    return close_ongoing_trades(
+        ongoing_trades, strike_option_type_exit_price_dict, current_time, data
+    )
 
 
-def buy_alice_blue_trades(
-    strike_quantity_dict, symbol, expiry: datetime.date, nfo_type
-):
+def buy_alice_blue_trades(self, data, quantity, expiry: datetime.date, nfo_type):
     """
     assumptions
      all trades to be executed should belong to same:
@@ -108,39 +126,48 @@ def buy_alice_blue_trades(
       call type [ for ex: either CE, PE ]
       nfo type [ for ex: either future or option]
     """
+    from main import telegram_bot
+    from apis.utils import STATUS
 
     alice = get_alice_blue_obj()
-    buy_order_action_id_list = []
 
     if isinstance(expiry, str):
         expiry = datetime.datetime.strptime(expiry, "%d %b %Y").date()
 
-    for strike, quantity in strike_quantity_dict.items():
-        instrument = alice.get_instrument_for_fno(
-            symbol=symbol,
-            expiry_date=expiry,
-            is_fut=nfo_type != "option",
-            strike=strike,
-            is_CE=quantity > 0,
-        )
+    quantity = quantity or data["quantity"]
+    instrument = alice.get_instrument_for_fno(
+        symbol=data["symbol"],
+        expiry_date=expiry,
+        is_fut=nfo_type != "option",
+        strike=data["strike"],
+        is_CE=quantity > 0,
+    )
 
-        place_order_response = alice.place_order(
-            transaction_type=TransactionType.Buy,
-            instrument=instrument,
-            quantity=quantity if quantity > 0 else (-1 * quantity),
-            order_type=OrderType.Market,
-            product_type=ProductType.Delivery,
-            price=0.0,
-            trigger_price=None,
-            stop_loss=None,
-            square_off=None,
-            trailing_sl=None,
-            is_amo=False,
-        )
-        buy_order_action_id_list.append(place_order_response["data"]["oms_order_id"])
+    place_order_response = alice.place_order(
+        transaction_type=TransactionType.Buy,
+        instrument=instrument,
+        quantity=quantity if quantity > 0 else (-1 * quantity),
+        order_type=OrderType.Market,
+        product_type=ProductType.Delivery,
+        price=0.0,
+        trigger_price=None,
+        stop_loss=None,
+        square_off=None,
+        trailing_sl=None,
+        is_amo=False,
+    )
 
-    for order_id in buy_order_action_id_list:
-        return get_order_status(alice, order_id)
+    order_id = place_order_response["data"]["oms_order_id"]
+    for _ in range(1, 10):
+        order_status = alice.get_order_history(order_id)["data"][0]
+        if order_status["order_status"] == STATUS.COMPLETE:
+            data["entry_price"] = order_status["average_price"]
+            return self.create_object(data, kwargs={})
+
+    capture_exception(Exception(order_status["rejection_reason"], order_status))
+
+    telegram_bot.send_message(chat_id="1229129389", text=order_status)
+    log.warning(alice.get_order_history(order_id)["data"][0])
 
 
 def get_order_status(alice, order_id):
