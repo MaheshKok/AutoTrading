c926c9561ba98cf85a0d2d8adf9f862b124dfb5b
MaheshKok <maheshkokare100@gmail.com>
2022-09-06 11:16:36 +0100

    added pool_pre_ping

--- extensions/database/__init__.py
+++ extensions/database/__init__.py
@@ -10,8 +10,16 @@ def register_db(app):
         #     "SQLALCHEMY_DATABASE_URI"
         # ] = "postgres+psycopg2://postgres:password@localhost:54322/paper_trading"
         # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-        app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://maheshkokare100:v2_3titR_UxWyXXER94VqFkMUWpyxwyz@db.bit.io/maheshkokare100/current_db"
+        app.config[
+            "SQLALCHEMY_DATABASE_URI"
+        ] = "postgresql://maheshkokare100:v2_3titR_UxWyXXER94VqFkMUWpyxwyz@db.bit.io/maheshkokare100/current_db"
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 1800}
+        app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+            "pool_recycle": 3600,
+            "pool_pre_ping": True,
+            "pool_size": 20,
+            "max_overflow": 0,
+            "pool_timeout": 30,
+        }
         extensions.db.init_app(app)
         extensions.db.create_all()
